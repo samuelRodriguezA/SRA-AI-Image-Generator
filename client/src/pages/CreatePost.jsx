4fcf7e241df80048f6870af0f08368b790b5ae73
samuelRodriguezA <srodriguezarmesto@gmail.com>
2024-04-01 10:55:22 -0400

    server changes

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -56,7 +56,7 @@ const CreatePost = () => {
     if (form.prompt && form.photo) {
       setLoading(true);
       try {
-        const response = await fetch("https://rich-gold-giraffe-tux.cyclic.app/api/v1/post", {
+        const response = await fetch("https://image-generator-backend-ne64.onrender.com/api/v1/post", {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -26,7 +26,7 @@ const Home = () => {
     setLoading(true);
 
     try {
-      const response = await fetch('https://rich-gold-giraffe-tux.cyclic.app/api/v1/post', {
+      const response = await fetch('https://image-generator-backend-ne64.onrender.com/api/v1/post', {
         method: 'GET',
         headers: {
           'Content-Type': 'application/json',
